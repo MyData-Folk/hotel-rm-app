0c56df0b967ec84ec9b6dedea49de8f422ed22dd
Galizi16 <galizilemgalizi@gmail.com>
2025-09-28 10:30:18 +0200

    Build final et correct des CSS Tailwind pour admin et frontend

---
+++ admin/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}" // Scanne tous les fichiers .html et .js à la racine de ce dossier
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
